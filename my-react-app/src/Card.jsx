bc6e6b94c7118ce86e484cb2975d3cd7446ee7f4
Ibrahim Salidu Kamara <ibrahimsallieuk@gmail.com>
2024-02-03 14:06:40 +0000

    React app 03-02-2024

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+    const food1 = `Orange`;
+    const food2 = `Banana`
+        return(
+             <div className="card">
+                <img className="card-image" src="https://yt3.ggpht.com/yti/AGOGRCok35HLL4BVhJjAFyKBFIBHVXrE2kwXW-KjxnvBvQ=s88-c-k-c0x00ffffff-no-rj" alt="card image not available" />
+                <h2 className="card-title">Ibrahim Salieu Kamara</h2>
+                <p className="card-text">I am an engineer</p>
+
+             </div>
+        );
+      }
+      export default Card;
+      
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return(
+        <footer>
+            <p>@copy;  KIE Estate {new Date().getFullYear()}</p>
+        </footer>
+  )
+}
+export default Footer
+
